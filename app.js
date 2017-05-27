8e7770728148ba928b9e205f462fb28ce0911463
Sylvain Lavoie <sylvoie.qc@icloud.com>
2017-05-27 17:57:13 -0400

    Ajout info dans helloworld

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var server_ip_address = process.env.HELLOWORLD_PORT_8080_TCP_ADDR || '127.0.0.1'
 app.get('/', function(req, res) {
    var pid = process.pid;
    var plateforme = process.platform;
-   var hostname = process.env.HOSTTYPE;
+   var hostname = process.env.HOSTNAME;
    res.send('Hello world sur from hostname : ' + hostname + ' PID ' + pid + ' plateforme ' + plateforme);
 });
 
