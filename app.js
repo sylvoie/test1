0bd7c020f8a99815291faa476374b1d37f4cbd8a
Sylvain Lavoie <sylvoie.qc@icloud.com>
2017-05-27 16:32:37 -0400

    Retrait  ADDR de Openshift

--- app.js
+++ app.js
@@ -13,6 +13,6 @@ app.get('/', function(req, res) {
    res.send('Hello world!!!');
 });
 
-app.listen(process.env.HELLOWORLD_PORT_8080_TCP_ADDR_PORT, process.env.HELLOWORLD_PORT_8080_TCP_ADDR,function(){
-   console.log('Listening on : ' + process.env.HELLOWORLD_PORT_8080_TCP_ADDR + ' pprt : ' + process.env.HELLOWORLD_PORT_8080_TCP_ADDR_PORT);
+app.listen(process.env.HELLOWORLD_PORT_8080_TCP_ADDR_PORT, function(){
+   console.log("Listening on port : " + process.env.HELLOWORLD_PORT_8080_TCP_ADDR);
 });
