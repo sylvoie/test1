07fb926c6e4208d39abfc246327e3212bffd0b56
Sylvain Lavoie <sylvoie.qc@icloud.com>
2017-05-27 17:34:16 -0400

    Correction env var PORT IP

--- app.js
+++ app.js
@@ -5,8 +5,8 @@
 const express = require('express');
 const app = express();
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.HELLOWORLD_PORT_8080_TCP_PORT || 8080;
+var server_ip_address = process.env.HELLOWORLD_PORT_8080_TCP_ADDR || '127.0.0.1';
 
 // ======================================================
 // ROUTES
